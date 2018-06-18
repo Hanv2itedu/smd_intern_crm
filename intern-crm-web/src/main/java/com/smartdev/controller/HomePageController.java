0ea456ed16f12c336f1b48dcfdf260d599da544a
daukya <37116821+daukya@users.noreply.github.com>
2018-06-18 16:40:47 +0700

    add customer

--- intern-crm-web/src/main/java/com/smartdev/controller/HomePageController.java
+++ intern-crm-web/src/main/java/com/smartdev/controller/HomePageController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-
 public class HomePageController {
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String login(){
@@ -15,4 +14,5 @@ public class HomePageController {
 	public String home(){
 		return "home";
 	}
+
 }
